5e8169ed64804c66253e6710f84ce186fd8ce296
Pawel Wos <pawelwos@gmail.com>
2024-02-05 21:40:19 +0100

    Year month navigation WIP

--- main.go
+++ main.go
@@ -1,33 +1,68 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/pawelwos/go-calendar"
 )
 
+func addDate(y int, m int) string {
+	if m < 12 {
+		return fmt.Sprintf("%v/%v", y, (m + 1))
+	} else {
+		return fmt.Sprintf("%v/%v", (y + 1), 1)
+	}
+}
+
+func subDate(y int, m int) string {
+	if m > 1 {
+		return fmt.Sprintf("%v/%v", y, (m - 1))
+	} else {
+		return fmt.Sprintf("%v/%v", (y - 1), 12)
+	}
+}
+
 func main() {
 
 	r := gin.Default()
 	r.Static("/css", "./static/css")
+	r.SetFuncMap(template.FuncMap{
+		"addDate": addDate,
+		"subDate": subDate,
+	})
 	r.LoadHTMLGlob("src/html/*")
 
 	r.GET("/", func(c *gin.Context) {
-
-		var cal = calendar.Create(time.Now().Year(), int(time.Now().Month()))
+		year := time.Now().Year()
+		month := int(time.Now().Month())
+		var cal = calendar.Create(year, month)
 		head := calendar.GetHead()
 		body := cal.GetBody()
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"thead": head,
 			"tbody": body,
+			"year":  year,
+			"month": month,
 		})
 	})
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	r.GET("/:year/:month", func(c *gin.Context) {
+		year, _ := strconv.Atoi(c.Param("year"))
+		month, _ := strconv.Atoi(c.Param("month"))
+		var cal = calendar.Create(year, month)
+		head := calendar.GetHead()
+		body := cal.GetBody()
+		fmt.Printf("%v, %v", year, month)
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"thead": head,
+			"tbody": body,
+			"year":  year,
+			"month": month,
 		})
 	})
 	r.Run("0.0.0.0:3000") // listen and serve on 0.0.0.0:8080
